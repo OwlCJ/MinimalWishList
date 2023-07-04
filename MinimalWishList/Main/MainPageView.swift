cd8ca49980469ed55e08813324bde57f4c1a309a
OwlCJ <pcj0308@gmail.com>
2023-07-04 11:11:03 +0900

    Mod: change root view & resizing UI

---
+++ MinimalWishList/Main/MainPageView.swift
@@ -0,0 +1,18 @@
+//
+//  MainPageView.swift
+//  MinimalWishList
+//
+//  Created by 박철진 on 6/29/23.
+//
+
+import SwiftUI
+
+struct MainPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPageView()
+}
